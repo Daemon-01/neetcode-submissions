e7186315a3169f53a5a99e19ffaaa8d721945507
Daemon-01 <parthrawatas@gmail.com>
2026-08-23 07:02:15 +0530

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        self.count = 0
+
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        def dfs(r):
+            if r == n:
+                self.count += 1
+                return 
+            
+            for c in range(n):
+                if c in cols or (r+c) in posDiag or (r-c) in negDiag:
+                    continue
+                
+                cols.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+
+                dfs(r+1)
+
+                cols.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+        dfs(0)
+        return self.count
